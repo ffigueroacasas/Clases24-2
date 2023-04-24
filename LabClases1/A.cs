3624722996549a20a51e347e1826ab052593e41d
ffigueroacasas <93206842+ffigueroacasas@users.noreply.github.com>
2023-04-24 18:11:05 -0300

    Add files via upload

---
+++ LabClases1/A.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LabClases1
+{
+    public class A
+    {
+        string _NombreInstancia;
+        public A()
+        {
+            _NombreInstancia = "Instancia sin nombre";
+        }
+        public A(string nombre)
+        {
+            _NombreInstancia = nombre;
+        }
+        public string NombreInstancia {
+
+            get {
+
+                return _NombreInstancia;
+
+            }
+
+            set {
+
+                _NombreInstancia = value;
+
+            }
+
+        }
+        public void mostrarNombre()
+        {
+            Console.WriteLine(_NombreInstancia);
+        }
+        public void M1()
+        {
+            Console.WriteLine("Se invoco el metodo M1");
+        }
+        public void M2()
+        {
+            Console.WriteLine("Se invoco el metodo M2");
+        }
+        public void M3()
+        {
+            Console.WriteLine("Se invoco el metodo M3");
+        }
+
+    }
+
+}
---
+++ LabClases1/B.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClases1
+{
+    public class B : A
+    {
+        public B() : base("Instancia de B")
+        {
+        }
+        public void M4()
+        {
+            Console.WriteLine("Metodo del hijo invocado");
+        }
+    }
+}
